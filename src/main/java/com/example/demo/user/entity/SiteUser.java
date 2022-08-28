45719a30aaed1026961f8e59f7360418be140469
hyujikoh <dvum0045@gmail.com>
2022-08-28 15:07:41 +0900

    queryDsl 사용위한 초기 셋팅

---
+++ src/main/java/com/example/demo/user/entity/SiteUser.java
@@ -0,0 +1,25 @@
+package com.example.demo.user.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+}
