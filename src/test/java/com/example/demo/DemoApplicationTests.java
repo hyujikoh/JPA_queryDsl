a6195bdbcb402e74aff54dc96792167ef4112e3d
hyujikoh <dvum0045@gmail.com>
2022-08-29 14:19:43 +0900

    각 테스트 마다 Transactional 처리 init 데이터를 제외하고 전부 롤백 처리

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,10 +6,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 
 @SpringBootTest
+@ActiveProfiles("test") // 테스트 모드 활성화
+// 이렇게 클래스 @Transactional를 붙이면, 클래스의 각 테스트케이스에 전부 @Transactional 붙은 것과 동일 , T1 케이스 실행시 롤백이 된다
+// @Test + @Transactional 조합은 자동으로 롤백을 유발시킨다.
+@Transactional
 class DemoApplicationTests {
 
     @Autowired
@@ -18,23 +24,24 @@ class DemoApplicationTests {
     @Test
     @DisplayName("회원생성")
     void t1() {
-       SiteUser u1 = SiteUser.builder()
-               .username("user3")
-               .password("{noop}123")
-               .email("user3@test.com")
-               .build();
-        SiteUser u2 = SiteUser.builder()
+        SiteUser u3 = SiteUser.builder()
+                .username("user3")
+                .password("{noop}1234")
+                .email("user3@test.com")
+                .build();
+
+        SiteUser u4 = SiteUser.builder()
                 .username("user4")
-                .password("{noop}123")
+                .password("{noop}1234")
                 .email("user4@test.com")
                 .build();
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        userRepository.saveAll(Arrays.asList(u3, u4));
     }
 
     @Test
     @DisplayName("회원생성")
     void t2() {
-        userRepository.getQslUser(1L);
+        userRepository.getQslUser(4L);
     }
 
 }
